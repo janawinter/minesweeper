0929049ea49855571ded6a7e11738105f34daf81
Jana Winter <janawhitta@gmail.com>
2016-06-20 18:31:00 +1200

    add- remove hidden function

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+
+    var childElements = document.getElementsByClassName('hidden')
+    console.log (childElements);
+    for (var i = 0 ; i < childElements.length; i++){
+ addListeners (childElements.item(i))
+    }
+}
+function addListeners (element){
+console.log (element);
+  element.addEventListener ('click', showCell)
+}
+function showCell (evt){
+  var cell = evt.currentTarget;
+  cell.classList.remove('hidden');
 }
