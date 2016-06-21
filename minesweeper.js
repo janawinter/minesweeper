d2dd3c530a86eabd5ec37dce38249f37134af7a3
Jana Winter <janawhitta@gmail.com>
2016-06-21 17:46:57 +1200

    latest lib.js

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', startGame);
 
+var board = {
+  cells: []
+}
+
 function startGame() {
-  var childElements = document.getElementsByClassName('hidden');
+  var childElements = document.getElementsByClassName('board')[0].children;
   //console.log (childElements);
   for (var i = 0 ; i < childElements.length; i++) {
     addListeners(childElements.item(i));
     addCellToBoard(childElements.item(i));
   }
+  for(var i=0 ; i < board.cells.length; i++) {
+    countSurroundingMines(board.cells[i]);
+  }
+}
+
+function countSurroundingMines(element) {
+  var result = getSurroundingCells(element.row, element.col);
+  board.cells.element.surroundingMines = result;
 }
 
 function addListeners(element) {
@@ -24,35 +36,34 @@ function markCell(evt) {
   evt.target.classList.toggle('marked');
 }
 
-var board = {
-  cells: []
-}
-
 function getRow(element) {
     for (var i=0; i < element.classList.length; i++) {
       var boxes= element.classList[i];
       if (boxes.indexOf("row") > -1) {
         var number = boxes.split("-");
         //console.log (number[1]);
-        return number[1];
+        return parseInt(number[1]);
     }
   }
 }
+
 function getCol(element) {
     for (var i=0; i < element.classList.length; i++) {
       var boxes= element.classList[i];
       if (boxes.indexOf("col") > -1) {
         var number = boxes.split("-");
         //console.log (number[1]);
-        return number[1];
+        return parseInt(number[1]);
     }
   }
 }
+
 function addCellToBoard(element) {
- var newCell = {
+  var newCell = {
     row: getRow(element),
     col: getCol(element),
     isMine:element.classList.contains("mine")
- }
-board.cells.push(newCell)
+  }
+  board.cells.push(newCell)
 }
+
